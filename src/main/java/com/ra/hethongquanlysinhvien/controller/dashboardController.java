29ec99e8538685019f090f5a177ddce9e4a834e1
Jicap <tranvanquoccode@gmail.com>
2025-07-15 20:56:07 +0900

    Fix case-sensitive issue with DashboardController.java

--- src/main/java/com/ra/hethongquanlysinhvien/controller/dashboardController.java
+++ src/main/java/com/ra/hethongquanlysinhvien/controller/dashboardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/dashboard")
-public class tempController {
+public class DashboardController {
     @Autowired
     private CourseService courseService;
 
